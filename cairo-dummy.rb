4fa97c145009df0b5c8be12846a77501104d168c
kou <kou@55590b84-c0e8-db11-88f6-0010dcdd6dc2>
2008-04-11 15:43:14 +0000

    * add dummy entries.
    git-svn-id: http://www.cozmixng.org/repos/rcairo-doc@89 55590b84-c0e8-db11-88f6-0010dcdd6dc2

--- cairo-dummy.rb
+++ cairo-dummy.rb
@@ -1,9 +1,17 @@
 module Cairo
   class Win32Surface < Surface
-    attr_reader :hdc, :image
+    attr_reader :initialize, :hdc, :image
+  end
+
+  class Win32PrintingSurface < Surface
+    attr_reader :initialize, :hdc
   end
 
   class QuartzSurface < Surface
-    attr_reader :cg_context
+    attr_reader :initialize, :cg_context
+  end
+
+  class QuartzImageSurface < Surface
+    attr_reader :initialize, :image
   end
 end
